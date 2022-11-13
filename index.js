536cd39ca29bf289ef9d2b45ed6a7a5f58f1f561
trdhu <trdhurba12@gmail.com>
2022-11-13 07:47:19 -0800

    index.js

--- index.js
+++ index.js
@@ -7,21 +7,69 @@ const itemsForm =document.querySelector("#items-form")
 
 //test the code.what is the return value and why?
 //console.log(itemsContainer)
+//console.log(itemsform)
 
 //add an event listener to form calling the event handler,createItem
 itemsForm.addEventListener('submit',createItem)
 
-function createItem(event) {
+function createItem() {
   event.preventDefault()
-  
-  //this event handler should create an new items object and persist data 
-  
+  console.log("submit form!!!")
+//this event handler should create anew items object and persist data
+
  const name = document.querySelector('#items-input').value
  const quantity = document.querySelector('#quantity-input').value
+ debugger;
+//Create an items object
+const newObj = {
+ //name: 
+ //quantity: 
+ //completed: 
+ }
 
- //event.target['items-input'].value
- //event.target["quantity-input"].value
- //Create an items object
+}
+//persist this data
+
+fetch (BASE_URL, {
+  method: 'post',
+  headers: {
+    'content-type': 'application/json'
+  },
+  body: JSON.stringify(newObj)
+})
+.then(resp =>resp.json())
+.then(data =>console.log(data))
+
+
+
+
+
+
+//review this code.identify that is mising?test your theories!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //persist this data
   
@@ -30,13 +78,13 @@ function createItem(event) {
     headers: {
       'content-type': 'application/json'
     },
-    body: JSON.stringify(newObj)
+    body: JSON.stringify(newitemObj)
   })
   .then(resp =>resp.json())
   .then(item=>renderItem(item))
   //.then(renderItem ) //refactored line 43
 
-}
+
   //Review code, test it.
   function renderItem(item) {
     const checkBox =document.createElement("input");
