c90b089e65c31f85ee0bdb2df76304c89058582a
trdhu <trdhurba12@gmail.com>
2022-11-28 13:45:40 -0800

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_URL ="http://localhost:3000/items";
+const BASE_URL = "http://localhost:3000/items";
 // select and save the items container element
 const itemsContainer =document.querySelector("#items-container")
 
@@ -21,58 +21,96 @@ function createItem() {
  const quantity = document.querySelector('#quantity-input').value
 
 //Create an items object
+
 const newObj = { //plain old javascript object
  name: name,
  quantity: quantity, 
  completed: false 
 }
 
-
 console.log ("newObj",newObj)
 //persist this data
 
-fetch (BASE_URL, {
-  method: 'post',
+fetch(BASE_URL, {
+  method: 'POST',
   headers: {
     'content-type': 'application/json'
   },
   body: JSON.stringify(newObj)
 })
 .then(resp =>resp.json())
-.then(data =>renderItem(data))
-
-//review this code.identify that is mising?test your theories!
-
-  
-
+.then(item => renderItem(item))
+//.then(renderItem ) //refactored line 42.
 }
-  //.then(renderItem ) //refactored line 43
-
-
-  //Review code, test it.
+//review this code.identify that is mising?test your theories!
   function renderItem(item) {
-    console.log('RENDER ITEM')
+    //creating a checkbox for each item
+    console.log(item)
+    console.log('RENDER ITEM' )
     const checkBox = document.createElement("input");
     checkBox.setAttribute("type","checkbox");
+     //checking status of items completed property.and if true,keeping the checkbox marked
     if (item.completed) {
       checkBox.checked = true;
     }
+  
+     //fix this code to pass the item object to the event handler
+    checkBox.addEventListener("click", () => {updateCompleted(item)
+    });
+     //remember that element id's should be unique
+    const listItem = document.createElement("li");
+    listItem.id = `items`;
+    listItem.textContent = `Name: ${item.name} - quantity: ${item.quantity} - bought: `;
 
-  //fix this code to pass the item object to the event handler
-    checkBox.addEventListener("click",updatecompleted);
-  }
+    const deleteBttn = document.createElement("button");
+    deleteBttn.textContent = "x";
+
+    deleteBttn.addEventListener('click', () => deleteItem(item))
+  
+    listItem.append(checkBox,deleteBttn);
+    itemsContainer.appendChild(listItem)
+    //test this method by calling in getItems method to render items DOM.
+}
   function updatecompleted(item) {
-  
-  }
-  function getItem() {
-  
-  }
+  console.log('UPDATE COMPLETED')
+  //this togic will use the the! operator to toggle the status of the property, completed.if it returns false, it wll update to true all vice versa.
+  let completed = (item.completed = !item.completed);
+  console.log(completed)
+  //if item.completed === true
+  //then assign item.completed = false
+  //if item.completed === false
+  //then assign item.completed = true
 
+  //make a fetch request to update data on server upon checkbox click
 
+  fetch(BASE_URL + `/${item.id}`, {
+    method: 'PATCH',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({ completed: completes})
+    })
+}  
+  //responsible for getting all the resources
+function getItem() {
+  //need to make a fetch request for all items from server
+  fetch(BASE_URL)
+  .then(resp => resp.json())
+  .then(items.forEach(renderItem))
+}
 
-
-
-
+function deleteItem(itemId){
+ //console.log(listItem)
+ // console.log(itemId)
+  fetch(`http://localhost:3000/items  /${item.id}`, {
+    method: 'DELETE',
+    headers: {
+      'content-Type':'application/json'
+    }
+  })
+   .then(res =>res.json())
+   .then(item =>console.log(item))
+}
 
 
 
